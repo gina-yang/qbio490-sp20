bdec77171cd3d53aeff106abbd07f5692f7733be
Gina Yang <ginayang@usc.edu>
2020-02-05 13:13:44 -0800

    cleaning script rowname replace

--- data/rnaseq_clean.R
+++ data/rnaseq_clean.R
@@ -1,5 +1,7 @@
 allcells <- read.csv("GSE81861_CRC_tumor_all_cells_COUNT.csv")
-genes <- allnames[1]
-for( i in genes ){
-  
+names <- allcells[1] # save rownames
+names <-as.vector(unlist(names)) # convert from list to character vector
+for( i in 1:length(names) ){
+  names[i] <- strsplit(names[i], "_")[[1]][2]
 }
+allcells[1]<-list(names)
